b62514602fc0e5ea16309da5823991b96ad2e932
Stuart Bowes <stuartbowes2000@gmail.com>
2022-08-10 11:08:27 +0100

    Add TextField widgets for Username and Password to login screen

--- lib/login.dart
+++ lib/login.dart
@@ -41,6 +41,20 @@ class _LoginPageState extends State<LoginPage> {
             const SizedBox(height: 120.0),
             // TODO: Remove filled: true values (103)
             // TODO: Add TextField widgets (101)
+            TextField(
+              decoration: const InputDecoration(
+                filled: true,
+                labelText: 'Username',
+              ),
+            ),
+            const SizedBox(height: 12.0),
+            TextField(
+              decoration: const InputDecoration(
+                filled: true,
+                labelText: 'Password',
+              ),
+              obscureText: true,
+            )
             // TODO: Add button bar (101)
           ],
         ),
