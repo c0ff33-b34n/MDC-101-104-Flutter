f1ea5b54cc8387345a44afdb366fdc9c8b926fa5
Stuart Bowes <stuartbowes2000@gmail.com>
2022-08-10 11:54:13 +0100

    Add a GridView with an empty Card

--- lib/home.dart
+++ lib/home.dart
@@ -56,9 +56,11 @@ class HomePage extends StatelessWidget {
           ),
         ],
       ),
-      // TODO: Add a grid view (102)
-      body: Center(
-        child: Text('You did it!'),
+      body: GridView.count(
+        crossAxisCount: 2,
+        padding: const EdgeInsets.all(16.0),
+        childAspectRatio: 8.0 / 9.0,
+        children: [Card()],
       ),
       resizeToAvoidBottomInset: false,
     );
